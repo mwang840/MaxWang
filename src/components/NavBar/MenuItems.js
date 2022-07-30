079a558c242936deac3bbded900ebc4ebbd739f5
mwang840 <maxwang@udel.edu>
2022-07-29 21:42:00 -0400

    menu and navigations

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Awards',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contracts',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
