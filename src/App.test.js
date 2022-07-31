63b9b24e0dfb62675ef483cdfee17b382b4a0a86
Maxwell Wang <maxwang@udel.edu>
2022-07-31 14:23:25 -0400

    fixed test to show bar

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the menu bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome/i);
+  const linkElement = screen.getByText(/Home/i);
+  const linkElementTwo = screen.getByText(/About/i);
+  const linkElementThree = screen.getByText(/Awards/i);
+  const linkElementFour = screen.getByText(/Projects/i);
+  const linkElementFive = screen.getByText(/Contacts/i);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElementTwo).toBeInTheDocument();
+  expect(linkElementThree).toBeInTheDocument();
+  expect(linkElementFour).toBeInTheDocument();
+  expect(linkElementFive).toBeInTheDocument();
 });
