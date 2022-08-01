ebe8abe1cd0de859ba36b88d429f801dc7f4202c
Maxwell Wang <maxwang@udel.edu>
2022-07-31 22:02:23 -0400

    button for now

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Button.css";
+
+const STYLES =[
+    "btn--primary",
+    "btn--outline"
+]
+
+const SIZES =[
+    "btn--medium",
+    "btn--large"
+]
+
+export const Button = {
+    
+}
