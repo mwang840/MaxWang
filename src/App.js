d7463f795ed06f69bdd2dbc7ca8404f017a7b635
mwang840 <maxwang@udel.edu>
2022-08-02 21:27:20 -0400

    styled image in center and centered text

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <img src={max} alt="Max" style={{width: 400, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
+      <h1>Welcome!</h1>
+      <p>My name is Maxwell Wang a junior studying computer science at the University of Delaware</p>
     </div>
   );
 }
