ecca9b66cb94d619aa3453842b31dc342d2f1edf
mwang840 <maxwang@udel.edu>
2022-08-03 16:53:29 -0400

    fixed puncuation

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ function App() {
       <NavBar></NavBar>
       <img src={max} alt="Max" style={{width: 250, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
       <h1>Welcome!</h1>
-      <p>My name is Maxwell Wang a undergradutate junior studying computer science at the University of Delaware.
+      <p>My name is Maxwell Wang, an undergradutate junior studying computer science at the University of Delaware.
       </p>
-      <p>I am currently contributing to an undergradutate research
-        project in human-computer interaction</p>
-
-      <p><small>This website was made with Javascript and the React.js framework.</small></p>
+      <p>If you want to learn more from, me, feel free to explore the different menu sections of my website</p>
+      <footer><small>This website was made with Javascript and the React.js framework.</small></footer>
       <p>Got any issues, submit it to the feedback button right on my website</p>
     </div>
   );
