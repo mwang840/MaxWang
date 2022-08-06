088bf96acd22f1fd8c0df56f7d8ff2e2615f5969
mwang840 <maxwang@udel.edu>
2022-08-05 21:36:28 -0400

    fixed title tag

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
       <NavBar>
         <Router/>
       </NavBar>
-      <img src={max} alt="Max" style={{width: 250, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
       <title>Welcome!</title>
-      <p>My name is Maxwell Wang, an undergradutate junior studying computer science at the University of Delaware.
+      <img src={max} alt="Max" style={{width: 250, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
+      <p>My name is Maxwell Wang, an undergradutate junior studying computer science at the University of Delaware .
       </p>
       <p>If you want to learn more from, me, feel free to explore the different menu sections of my website</p>
       <footer><small>This website was made with Javascript and the React.js framework.</small></footer>
