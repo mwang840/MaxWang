ba6539f1cb30342d8d598514058a911363dff59c
Maxwell Wang <maxwang@udel.edu>
2022-08-07 08:26:47 -0400

    constructed the home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return <div>
+        <p>My name is Maxwell Wang, an undergradutate junior studying computer science at the University of Delaware concentrating in artificial intelligence.
+      </p>
+      <p>Feel free to explore the different menu sections of my website</p>
+      <p>Got any issues, submit it to the feedback button right on my website</p>
+      <footer><small>This website was made with Javascript and the React.js framework.</small></footer>
+    </div>
+}
+
+export default Home;
