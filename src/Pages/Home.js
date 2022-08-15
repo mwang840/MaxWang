f2cc7c817e72901b5761c2571bb35e7d28020d33
mwang840 <maxwang@udel.edu>
2022-08-15 14:52:59 -0400

    fixed grammar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,12 +5,10 @@ import "./Home.css"
 function Home(){
     return <div>
        <h1>Welcome</h1>
-       <img src={max} alt="Max" style={{width: 250, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
+       <img src={max} alt="Max" style={{width: 300, height: 200, display:"absolute", top:0, left:0, right:10, bottom:0}}></img>
         <p>My name is Maxwell Wang, an undergradutate junior studying computer science at the University of Delaware concentrating in artificial intelligence. 
-          <br></br>Feel free to explore the different menu sections of my website.
+          <br></br>Feel free to explore the different menu sections on the top of my website.
       </p>
-
-      <p>Feel free to explore the different menu sections of my website</p>
       <p>If the website has any issues, submit it under the contacts page.</p>
       <br></br>
       <footer><small>This website was made with Javascript and the React.js framework.</small></footer>
