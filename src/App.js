942217367f36e4559733ab7caedeee1ea209b66f
Maxwell Wang <maxwang@udel.edu>
2022-08-14 21:27:35 -0400

    a new link popped up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Contacts from './Pages/Contacts';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/contacts" element={<Contacts/>}/>
       </Routes>
     </div>
   );
