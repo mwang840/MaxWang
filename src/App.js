d130a8ac884aae911b8fa3091ff6fd8884001836
mwang840 <maxwang@udel.edu>
2022-08-19 22:33:13 -0400

    set up router for projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Projects from './Pages/Projects';
 import Contacts from './Pages/Contacts';
 import { Routes, Route } from 'react-router-dom';
 import { faGithub, faLinkedin, faJs, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
@@ -16,6 +17,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/projects" element={<Projects/>}/>
           <Route path="/contacts" element={<Contacts/>}/>
       </Routes>
       <footer>
